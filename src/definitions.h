7031a2081f4eecd1c67672c82bd3163fa3581fc1
ALYSON FARIA JACOMINE <alyson.jacomine@hotmail.com>
2025-04-18 23:36:36 -0300

    Replace deprecated ASIO functions and raise minimum Boost version
    * Replace deprecated asio functions
    
    * Bump minimal Boost version

--- src/definitions.h
+++ src/definitions.h
@@ -1,12 +1,12 @@
-// Copyright 2022 The Forgotten Server Authors. All rights reserved.
+// Copyright 2023 The Forgotten Server Authors. All rights reserved.
 // Use of this source code is governed by the GPL-2.0 License that can be found in the LICENSE file.
 
-#ifndef FS_DEFINITIONS_H_877452FEC245450C9F96B8FD268D8963
-#define FS_DEFINITIONS_H_877452FEC245450C9F96B8FD268D8963
+#ifndef FS_DEFINITIONS_H
+#define FS_DEFINITIONS_H
 
 static constexpr auto STATUS_SERVER_NAME = "The Forgotten Server";
-static constexpr auto STATUS_SERVER_VERSION = "1.4.2";
-static constexpr auto STATUS_SERVER_DEVELOPERS = "The Forgotten Server Team";
+static constexpr auto STATUS_SERVER_VERSION = "10.98";
+static constexpr auto STATUS_SERVER_DEVELOPERS = "The Forgotten Server Team & ALS";
 
 static constexpr auto CLIENT_VERSION_MIN = 1097;
 static constexpr auto CLIENT_VERSION_MAX = 1098;
@@ -14,6 +14,9 @@ static constexpr auto CLIENT_VERSION_STR = "10.98";
 
 static constexpr auto AUTHENTICATOR_DIGITS = 6U;
 static constexpr auto AUTHENTICATOR_PERIOD = 30U;
+
+#define BOOST_ASIO_NO_DEPRECATED
+#define OPENSSL_NO_DEPRECATED
 
 #ifndef __FUNCTION__
 #define __FUNCTION__ __func__
@@ -26,8 +29,6 @@ static constexpr auto AUTHENTICATOR_PERIOD = 30U;
 #ifndef _USE_MATH_DEFINES
 #define _USE_MATH_DEFINES
 #endif
-
-#include <cmath>
 
 #ifdef _WIN32
 #ifndef NOMINMAX
@@ -51,13 +52,10 @@ static constexpr auto AUTHENTICATOR_PERIOD = 30U;
 #pragma warning(disable:4458) // declaration hides class member
 #endif
 
-#define strcasecmp _stricmp
-#define strncasecmp _strnicmp
-
 #ifndef _WIN32_WINNT
 // 0x0602: Windows 7
 #define _WIN32_WINNT 0x0602
 #endif
 #endif
 
-#endif
+#endif // FS_DEFINITIONS_H
